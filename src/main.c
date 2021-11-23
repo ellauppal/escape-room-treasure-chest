23df6f72a7b427afa696c9d17da043b14140bcaf
Ella Uppal <eauppal@uwaterloo.ca>
2021-11-22 20:38:07 -0500

    Final Game - without LED

--- src/main.c
+++ src/main.c
@@ -1,8 +1,8 @@
 // Written by Bernie Roehl, August 2021
 
-// #define COLOUR_GAME
-// #define PWM
-#define REED_SWITCH
+#define COLOUR_GAME
+#define PWM
+// #define REED_SWITCH
 
 #include <stdbool.h> // booleans, i.e. true and false
 #include <stdio.h>   // sprintf() function
@@ -10,14 +10,14 @@
 
 #include "ece198.h"
 
-
+/*
 void DisplaySensor(GPIO_TypeDef *port, uint16_t pin);
 
 void DisplaySensor(GPIO_TypeDef *port, uint16_t pin){
     
-    uint16_t period = 2000, prescale = 16;
+    uint16_t period = 100, prescale = 0;
 
-    if(!HAL_GPIO_ReadPin(port,pin))
+    if(!HAL_GPIO_ReadPin(port,pin)){
         __TIM2_CLK_ENABLE();  // enable timer 2
         TIM_HandleTypeDef pwmTimerInstance;  // this variable stores an instance of the timer
         InitializePWMTimer(&pwmTimerInstance, TIM2, period, prescale);   // initialize the timer instance
@@ -26,48 +26,34 @@ void DisplaySensor(GPIO_TypeDef *port, uint16_t pin){
 
         InitializePin(GPIOA, GPIO_PIN_5, GPIO_MODE_AF_PP, GPIO_NOPULL, GPIO_AF1_TIM2); // connect the motor to the timer output
 
-            while (true)
-            {
+            //while (true)
+           // {
                 for (uint32_t i = 0; i < period; ++i)
                     {
                     SetPWMDutyCycle(&pwmTimerInstance, TIM_CHANNEL_1, i);
                     HAL_Delay(5);
                     }
-            }     
- 
+          //  }     
+    }
+    
+   
     if(!HAL_GPIO_ReadPin(port,pin))
         SerialPutc('X');
     else
-        SerialPutc('_');
+        SerialPutc('_'); 
 }
 
+*/
 
 int main(void)
 {
     HAL_Init(); // initialize the Hardware Abstraction Layer
 
-    // Peripherals (including GPIOs) are disabled by default to save power, so we
-    // use the Reset and Clock Control registers to enable the GPIO peripherals that we're using.
-
     __HAL_RCC_GPIOA_CLK_ENABLE(); // enable port A (for the on-board LED, for example)
     __HAL_RCC_GPIOB_CLK_ENABLE(); // enable port B (for the rotary encoder inputs, for example)
     __HAL_RCC_GPIOC_CLK_ENABLE(); // enable port C (for the on-board blue pushbutton, for example)
 
-    // initialize the pins to be input, output, alternate function, etc...
-
-    //InitializePin(GPIOA, GPIO_PIN_5, GPIO_MODE_OUTPUT_PP, GPIO_NOPULL, 0);  // on-board LED
-
-    // note: the on-board pushbutton is fine with the default values (no internal pull-up resistor
-    // is required, since there's one on the board)
-
-    // set up for serial communication to the host computer
-    // (anything we write to the serial port will appear in the terminal (i.e. serial monitor) in VSCode)
-
     SerialSetup(9600);
-
-    // as mentioned above, only one of the following code sections will be used
-    // (depending on which of the #define statements at the top of this file has been uncommented)
-
 
 // initalizing LEDs
     InitializePin(GPIOA, GPIO_PIN_5 | GPIO_PIN_6 | GPIO_PIN_7, GPIO_MODE_OUTPUT_PP, GPIO_NOPULL, 0);
@@ -78,12 +64,10 @@ int main(void)
     
     InitializePin(GPIOC, GPIO_PIN_13, GPIO_MODE_INPUT, GPIO_NOPULL, 0); 
 
-    InitializePin(GPIOA, GPIO_PIN_6, GPIO_MODE_INPUT, GPIO_PULLUP, 0); // start button
+  //  InitializePin(GPIOA, GPIO_PIN_6, GPIO_MODE_INPUT, GPIO_PULLUP, 0); // start button
 
     InitializePin(GPIOA, GPIO_PIN_8, GPIO_MODE_INPUT, GPIO_PULLUP, 0); // start button
     InitializePin(GPIOB, GPIO_PIN_10 |  GPIO_PIN_3 | GPIO_PIN_4 | GPIO_PIN_5, GPIO_MODE_INPUT, GPIO_PULLUP, 0); // colour buttons
-
-
 
 #ifdef REED_SWITCH
     while(1){
@@ -98,7 +82,15 @@ int main(void)
 
     // Wait for the user to push button, then blink the LED.
 
+    bool play = true;
+
     bool led = true ;
+
+    int val = 0;
+
+while (play){
+    
+
     while (led){
         //  GPIOA, GPIO_PIN_5 == green led
         //  GPIOA, GPIO_PIN_6 == yellow led 
@@ -159,7 +151,6 @@ int main(void)
         //  GPIOB, GPIO_PIN_5 == yellow button
         //  GPIOB, GPIO_PIN_3 == green button
     
-    int val = 0;
     int arr[7];
     int real_arr[7]={1,2,1,3,2,3,3};
     char buff[100];
@@ -201,6 +192,8 @@ int main(void)
         HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
         HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_7);
         HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_6);
+
+        play = false;
     }
 
     else {
@@ -210,9 +203,12 @@ int main(void)
         
         HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_6);
 
+        led = true;
+
+        val = 0;
 
     }
-
+}
 
 #endif
 
@@ -229,7 +225,7 @@ int main(void)
 
     InitializePin(GPIOA, GPIO_PIN_1, GPIO_MODE_AF_PP, GPIO_NOPULL, GPIO_AF1_TIM2); // connect the motor to the timer output
 
-   // if (val == 7) {
+   if (val == 7) {
         while (true)
         {
             for (uint32_t i = 0; i < period; ++i)
@@ -238,7 +234,7 @@ int main(void)
                 HAL_Delay(5);
                 }
         }
-   // }
+    }
 
     
 /*
